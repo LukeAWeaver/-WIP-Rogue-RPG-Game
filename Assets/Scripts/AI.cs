f34b431cb5cda4ea5e300558b8fc92c98be07f84
LukeAWeaver <l501w882@ku.edu>
2017-06-18 15:32:30 -0500

    First Real Commit

---
+++ Assets/Scripts/AI.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+//using UnityEditor;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class AI : MonoBehaviour {
+    bool turn;
+    int ctr;
+
+
+
+	// Use this for initialization
+	void Start () {
+        turn = false;
+        ctr = 359;
+        /*var original = EditorBuildSettings.scenes;
+        var newSettings = new EditorBuildSettingsScene[original.Length + 1];
+        System.Array.Copy(original, newSettings, original.Length);
+        var sceneToAdd = new EditorBuildSettingsScene("Assets/level1", false);
+        newSettings[newSettings.Length - 1] = sceneToAdd;
+        EditorBuildSettings.scenes = newSettings;
+        */
+    }
+
+
+
+    // Update is called once per frame
+    void Update() {
+
+        ctr++;
+        if (ctr % 360 == 0)
+        {
+            if(ctr%720 ==0)
+            {
+
+                //SceneManager.LoadScene("level1");
+            }
+            turn = !turn;
+            if (!turn)
+            {
+                Flip("left");
+            }
+            else
+                Flip("right");
+        }
+        if (turn)
+        {
+            transform.Translate(-.04f, 0f, 0f); //left
+        }
+        else
+        {
+            transform.Translate(.04f, 0f, 0f); //right
+
+        }
+
+
+
+    }
+    public void Flip(string flip)
+    {
+        var theScale = transform.localScale;
+        if (flip == "left")
+        {
+            if (theScale.x < -.1f)
+                theScale.x = -theScale.x;
+        }
+        if (flip == "right")
+        {
+            if (theScale.x > .1f)
+                theScale.x = -theScale.x;
+        }
+        transform.localScale = theScale;
+    }
+}
---
+++ Assets/Scripts/Player1Controls.cs
@@ -0,0 +1,122 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player1Controls : MonoBehaviour
+{
+    private float velocity = 1f;
+    public float vlimit;
+    public float hlimit;
+
+    //public GameObject coin;
+   // public GameObject collisionObject;
+    //public GameObject smallObject;
+
+
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+        /* CONTROLLER SUPPORT
+        vlimit = Input.GetAxis("Vertical");
+        hlimit = Input.GetAxis("Horizontal");
+        if(Input.GetAxis("Vertical") <.1f && Input.GetAxis("Vertical") > -.1f)
+        {
+            transform.Translate(0f, Input.GetAxis("Vertical"), 0f);
+
+        }
+        else if (Input.GetAxis("Vertical") > .1f )
+        {
+            vlimit = .1f;
+            transform.Translate(0f, vlimit, 0f);
+        }
+        else if (Input.GetAxis("Vertical") < -.1f)
+        {
+            vlimit = -.1f;
+            transform.Translate(0f, vlimit, 0f);
+
+        }
+        if (Input.GetAxis("Horizontal") < .1f && Input.GetAxis("Horizontal") > -.1f)
+        {
+            transform.Translate(Input.GetAxis("Horizontal"), 0f, 0f);
+
+        }
+
+        else if (Input.GetAxis("Horizontal") > .1f)
+        {
+            hlimit = -.1f;
+            transform.Translate(hlimit, 0f, 0f);
+
+        }
+        else if (Input.GetAxis("Horizontal") < -.1f)
+        {
+            hlimit = .1f;
+            transform.Translate(hlimit, 0f, 0f);
+
+        }
+
+    */
+
+
+
+        if (Input.GetKey("right"))
+        {
+            Flip("right");
+            transform.Translate(velocity, 0f, 0f);
+        }
+        if (Input.GetKey("left"))
+        {
+            Flip("left");
+            transform.Translate(-velocity, 0f, 0f);
+        }
+        if (Input.GetKey("up"))
+        {
+            transform.Translate(0f, velocity, 0f);
+        }
+        if (Input.GetKey("down"))
+        {
+            transform.Translate(0f, -velocity, 0f);
+        }
+    }
+    public void Flip(string flip)
+    {
+        var theScale = transform.localScale;
+        if (flip == "right")
+        {
+            if (theScale.x < -.1f)
+                theScale.x = -theScale.x;
+        }
+        if (flip == "left")
+        {
+            if (theScale.x > .1f)
+                theScale.x = -theScale.x;
+        }
+        transform.localScale = theScale;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        /*
+        if (collision.gameObject.name == "16kg")
+        {
+            collisionObject.SetActive(false);
+            smallObject.SetActive(true);
+            velocity = .1f;
+            Debug.Log(collision.gameObject.name + " detected!");
+        }
+        if (collision.gameObject.name == "16g")
+        {
+            smallObject.SetActive(true);
+        }
+        */
+
+    }
+
+
+}
