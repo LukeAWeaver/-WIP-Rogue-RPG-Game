9c342cbb9d7bc23bd544f2e12ba36ab7af97d9fc
LukeAWeaver <l501w882@ku.edu>
2017-06-21 20:44:53 -0500

    Spawner

---
+++ Assets/Scripts/RandomMonsterSpawner.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomMonsterSpawner : MonoBehaviour {
+    public GameObject[] RandomMonsters;
+    public GameObject Knife;
+    public float spawnDelay = .1f;
+    public float spawnTime = 1f;
+    // Use this for initialization
+    void Start () {
+        InvokeRepeating("SpawnRandom", spawnDelay, spawnTime);
+    }
+
+    public void SpawnRandom()
+    {
+        int enemyIndex = Random.Range(0, RandomMonsters.Length);
+        var ThisEnemy = Instantiate(RandomMonsters[enemyIndex], transform.position, transform.rotation);
+        if(ThisEnemy.gameObject.name == "Goblin_Small(Clone)")
+        {
+            Instantiate(Knife);
+            this.Knife.GetComponent<weaponFollowNPC>().npc = ThisEnemy;
+            this.Knife.GetComponent<weaponFollowNPC>().CheckFlip = ThisEnemy.GetComponent<AI>();
+        }
+        if (ThisEnemy.gameObject.name == "Goblin_Big(Clone)")
+        {
+            Instantiate(Knife);
+            this.Knife.GetComponent<weaponFollowNPC>().npc = ThisEnemy;
+            this.Knife.GetComponent<weaponFollowNPC>().CheckFlip = ThisEnemy.GetComponent<AI>();
+        }
+
+    }
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
