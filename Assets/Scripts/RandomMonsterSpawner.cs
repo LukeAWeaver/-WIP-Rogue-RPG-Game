a85f27e97a4b01110d5117b76f7a8a967545fa9a
Long Chen <l115c442@ku.edu>
2017-06-22 00:12:41 -0500

    Fix spawn issue

--- Assets/Scripts/RandomMonsterSpawner.cs
+++ Assets/Scripts/RandomMonsterSpawner.cs
@@ -16,17 +16,20 @@ public class RandomMonsterSpawner : MonoBehaviour {
     {
         int enemyIndex = Random.Range(0, RandomMonsters.Length);
         var ThisEnemy = Instantiate(RandomMonsters[enemyIndex], transform.position, transform.rotation);
+        ThisEnemy.SetActive(true);
         if(ThisEnemy.gameObject.name == "Goblin_Small(Clone)")
         {
             Instantiate(Knife);
             this.Knife.GetComponent<weaponFollowNPC>().npc = ThisEnemy;
             this.Knife.GetComponent<weaponFollowNPC>().CheckFlip = ThisEnemy.GetComponent<AI>();
+            this.Knife.SetActive(true);
         }
         if (ThisEnemy.gameObject.name == "Goblin_Big(Clone)")
         {
             Instantiate(Knife);
             this.Knife.GetComponent<weaponFollowNPC>().npc = ThisEnemy;
             this.Knife.GetComponent<weaponFollowNPC>().CheckFlip = ThisEnemy.GetComponent<AI>();
+            this.Knife.SetActive(true);
         }
 
     }
