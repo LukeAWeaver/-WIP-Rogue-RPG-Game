563e488d117c982bfb5ddf70249065b807011c5a
py930828 <cl930828@gmail.com>
2017-06-27 23:41:50 -0500

    Add files via upload

--- Assets/Scripts/AI/AI.cs
+++ Assets/Scripts/AI/AI.cs
@@ -10,41 +10,71 @@ public class AI : MonoBehaviour {
     public GameObject player;
     public goblinInterface Thisgoblin;
     public GameObject npc;
+    public bool inSight;
+    public float range;
+    public int counter;
+    public float xVelocity;
+    public float yVelocity;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
+        counter = 0;
+        InvokeRepeating("movement", 0, .03f);
+    }
+
+    void movement ()
+    {
+        var target = player.transform.position;
+        var gp = Thisgoblin.transform.position;
+        range = Mathf.Sqrt((target.x - gp.x)* (target.x - gp.x) * (target.y - gp.y)* (target.y - gp.y));
+
+        if( range < 2 )
+        {
+            inSight = true;
+        }
+        else
+        {
+            if (counter % 60 ==0)
+            {
+                xVelocity = Random.Range(-0.03f, 0.03f);
+                yVelocity = Random.Range(-0.03f, 0.03f);
+            }
+            transform.Translate(xVelocity, yVelocity, 0f);
+            inSight = false;
+            counter++;
+        }
     }
 
 
 
     // Update is called once per frame
-    void Update() 
-{
+    void Update() {
         if(Thisgoblin.hp ==0)
         {
             npc.SetActive(false);
         }
         var target = player.transform.position;
-        if(transform.position.x > target.x)
+        if (inSight)
         {
-            Flip("right");
-            transform.Translate(-Thisgoblin.ms, 0f, 0f);
+            if (transform.position.x > target.x)
+            {
+                Flip("right");
+                transform.Translate(-Thisgoblin.ms, 0f, 0f);
+            }
+            else if (transform.position.x < target.x)
+            {
+                Flip("left");
+                transform.Translate(Thisgoblin.ms, 0f, 0f);
+            }
+            if (transform.position.y > target.y)
+            {
+                transform.Translate(0f, -Thisgoblin.ms, 0f);
+            }
+            else
+            {
+                transform.Translate(0f, Thisgoblin.ms, 0f);
+            }
         }
-        else if (transform.position.x < target.x)
-        {
-            Flip("left");
-            transform.Translate(Thisgoblin.ms, 0f, 0f);
-
-        }
-        if(transform.position.y > target.y)
-        {
-            transform.Translate(0f, -Thisgoblin.ms, 0f);
-        }
-        else
-        {
-            transform.Translate(0f, Thisgoblin.ms, 0f);
-        }
-
 
 
 
