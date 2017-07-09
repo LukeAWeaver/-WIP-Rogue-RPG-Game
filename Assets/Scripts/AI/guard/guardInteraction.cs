f5592d1c85bc8a2a05b9a9024509e579590f2212
LukeAWeaver <l501w882@ku.edu>
2017-07-09 17:19:36 -0500

    dialog boxes

---
+++ Assets/Scripts/AI/guard/guardInteraction.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class guardInteraction : MonoBehaviour {
+    private string dialogue0;
+    private string dialogue1;
+    private string randomlyChosenDialogue;
+    private int randomlyChosenNumberForDialogue;
+    private string guard;
+    private DialogueManager dManager;
+	// Use this for initialization
+	void Start () {
+        guard = "guard";
+        dManager = FindObjectOfType<DialogueManager>();
+        dialogue0 = "Guard: Be careful, many heroes have delved down there but none have yet to return.";
+        dialogue1 = "Guard: Do you have any relatives? You look a lot like the last guy to go down there.";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        randomlyChosenNumberForDialogue = Random.Range(0, 2);
+        if (randomlyChosenNumberForDialogue == 0)
+            randomlyChosenDialogue = dialogue0;
+        else if(randomlyChosenNumberForDialogue == 1)
+        {
+            randomlyChosenDialogue = dialogue1;
+        }
+	}
+    private void OnTriggerStay(Collider other)
+    {
+        if(other.gameObject.name == "Knight_Player")
+        {
+            if(Input.GetKeyUp(KeyCode.Space))
+            {
+                dManager.ShowBox(randomlyChosenDialogue,guard);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/GUI/DialogueManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class DialogueManager : MonoBehaviour {
+    public GameObject dBox;
+    public GameObject guardIcon;
+    public Text dtext;
+    public bool dialogActive;
+	// Use this for initialization
+	void Start () {
+        dialogActive = false;
+        dBox.SetActive(false);
+        guardIcon.SetActive(false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(dialogActive && Input.GetKeyDown(KeyCode.Space))
+        {
+            dBox.SetActive(false);
+            dialogActive = false;
+            guardIcon.SetActive(false);
+        }
+	}
+    public void ShowBox(string dialogue,string name)
+    {
+        dialogActive = true;
+        dBox.SetActive(true);
+        dtext.text = dialogue;
+        guardIcon.SetActive(true);
+        if(name == "guard")
+        {
+            guardIcon.SetActive(true);
+        }
+    }
+}
