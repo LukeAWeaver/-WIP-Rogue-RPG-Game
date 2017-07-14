f4142a05b6fd2a8d9964b7e6d20f94440f6ccf98
Rony Singh <rsingh.lx@gmail.com>
2017-07-14 18:54:03 -0500

    Ability 2 knockback added to Floor1 and overworld
    Need to add visuals and sound effects.

--- Assets/Scripts/Playable Characters/ability2Script.cs
+++ Assets/Scripts/Playable Characters/ability2Script.cs
@@ -5,17 +5,35 @@ using UnityEngine;
 public class ability2Script : MonoBehaviour {
 
     public GameObject knight;
+    bool check;
 
     void Start ()
     {
-
+      check = false;
     }
 
     // Update is called once per frame
     void Update ()
     {
-
-
+      if(Input.GetKeyDown("2") && GetComponentInParent<KnightStats>().energy > 20)
+      {
+        check = !check;
+      }
     }
 
-}
+    private void OnTriggerStay(Collider collision)
+    {
+
+      if(check && knight.GetComponent<KnightStats>().energy >20 && collision.gameObject.GetComponent<MonsterInterface>() != null)
+      {
+            collision.gameObject.GetComponent<MonsterInterface>().hp = collision.gameObject.GetComponent<MonsterInterface>().hp -knight.GetComponent<KnightStats>().AD;
+            collision.GetComponent<Rigidbody>().velocity += new Vector3(-knight.GetComponent<Player1Controls>().Ab1*20f, 0f, 0f);
+            check = false;
+      }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+
+    }
+  }
