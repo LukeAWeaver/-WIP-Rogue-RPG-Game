8bad6628f8a75c1da9059772d5dbaed5e606093b
LukeAWeaver <l501w882@ku.edu>
2017-07-29 01:49:07 -0500

    RNG terrain started

---
+++ Assets/lava.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class lava : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.GetComponent<KnightStats>() !=null || other.gameObject.GetComponent<MonsterInterface>() !=null)
+        {
+            other.GetComponent<Rigidbody>().velocity += new Vector3(0f, 15f, 0f);
+        }
+        if (other.gameObject.GetComponent<KnightStats>() != null && !other.gameObject.GetComponent<KnightStats>().isRecovering)
+        {
+            int changeInHP = PlayerPrefs.GetInt("currentHP") - 1;
+            other.GetComponent<Rigidbody>().velocity += new Vector3(0f, 2f, 0f);
+            PlayerPrefs.SetInt("currentHP", changeInHP);
+            other.gameObject.GetComponent<KnightStats>().isRecovering = true;
+
+
+        }
+    }
+}
