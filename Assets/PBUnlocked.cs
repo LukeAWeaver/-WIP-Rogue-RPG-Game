38a09f8d4304277414df4a8537d298bf4b8b7ee2
LukeAWeaver <l501w882@ku.edu>
2017-08-08 16:40:22 -0500

    +Unlock Potion brewer (save him and return him to village)

---
+++ Assets/PBUnlocked.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PBUnlocked : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		if(PlayerPrefs.GetInt("PBUnlocked") == 1)
+        {
+            gameObject.SetActive(true);
+        }
+        else
+        {
+            gameObject.SetActive(false);
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
