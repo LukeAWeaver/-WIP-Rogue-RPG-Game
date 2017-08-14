9cec74d61e8813fc8faa91fb01bb5fed84cc828c
LukeAWeaver <l501w882@ku.edu>
2017-08-13 22:22:31 -0500

    More skill tree progress

---
+++ Assets/constitution.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class constitution : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    private GameObject player;
+    // Use this for initialization
+    void Start()
+    {
+        player = FindObjectOfType<KnightStats>().gameObject;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (player.GetComponent<KnightStats>().UnlockAB1 == 1)
+        {
+            GetComponent<Image>().color = Color.white;
+        }
+        else
+        {
+            GetComponent<Image>().color = new Color32(128, 113, 113, 255);
+        }
+    }
+    //tooltip
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        gameObject.GetComponentInChildren<constDescription>().gameObject.GetComponent<Text>().text = "Constitution will help you survive longer. (Upgradable at the attribution trainer)"; //waaa this was crazy to code
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        gameObject.GetComponentInChildren<constDescription>().gameObject.GetComponent<Text>().text = "";
+    }
+}
---
+++ Assets/str.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class str : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    private GameObject player;
+    // Use this for initialization
+    void Start()
+    {
+        player = FindObjectOfType<KnightStats>().gameObject;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (player.GetComponent<KnightStats>().UnlockAB1 == 1)
+        {
+            GetComponent<Image>().color = Color.white;
+        }
+        else
+        {
+            GetComponent<Image>().color = new Color32(128, 113, 113, 255);
+        }
+    }
+    //tooltip
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        gameObject.GetComponentInChildren<strDescription>().gameObject.GetComponent<Text>().text = "Strength will help you hit harder. (Upgradable at the attribution trainer)"; //waaa this was crazy to code
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        gameObject.GetComponentInChildren<strDescription>().gameObject.GetComponent<Text>().text = "";
+    }
+}
